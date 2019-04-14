36cbfedcef330483e7f843aa6f486269974e5c6f
RacovitaMadalina <madalina.racovita@info.uaic.ro>
2019-04-15 00:08:06 +0300

    Added scheduler

--- mycinema/src/main/java/ro/fiipractic/mycinema/MycinemaApplication.java
+++ mycinema/src/main/java/ro/fiipractic/mycinema/MycinemaApplication.java
@@ -2,11 +2,17 @@ package ro.fiipractic.mycinema;
 
 import org.apache.log4j.BasicConfigurator;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.logging.Logger;
 
 @SpringBootApplication
+@EnableScheduling
 public class MycinemaApplication {
 
     static Logger log = Logger.getLogger("D://myCinema_log4j.log");
